059e8c9cb12b60682707d8dc21d676f1d1c325f2
Jennie K <47018372+JKGills12@users.noreply.github.com>
2019-07-28 22:17:50 -0700

    package-lock.json

--- liri.js
+++ liri.js
@@ -1,3 +1,10 @@
+// Liri takes in the following commands
+// concert-this
+// spotify-this-song 
+// movie-this
+// do-what-it-says
+
+
 require("dotenv").config();
 
 var keys = require("./keys.js");
