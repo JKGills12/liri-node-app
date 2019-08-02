d282443e921fbef1bed91d4e6b015da58e4ec375
Jennie K <47018372+JKGills12@users.noreply.github.com>
2019-08-02 12:35:37 -0700

    did not save changes before commiting, so re-committing -set comments for break down of steps

--- liri.js
+++ liri.js
@@ -7,5 +7,24 @@
 
 require("dotenv").config();
 
+// import stored keys
 var keys = require("./keys.js");
+
+// access keys info
+var spotify = new spotify(keys.spotify);
+
+
+// Liri takes in concert-this commands
+
+
+
+// Liri takes spotify-this-song commands
+
+
+// Liri takes movie-this commands
+
+
+// Liri take do-what-it-says commands
+
+
 
